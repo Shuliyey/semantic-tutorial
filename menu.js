eab28df29ccce67e57de6ab09ab9b26737e47b25
Zeyu Ye <z.ye@base2services.com>
2017-12-18 09:56:24 +1100

    update menu.html

---
+++ menu.js
@@ -0,0 +1,21 @@
+// $('.ui.menu .more_option.item').popup({
+//   inline   : true,
+//   hoverable: true,
+//   position : 'bottom left',
+//   // popup: $('.more_option.ui.popup'),
+//   delay: {
+//     show: 300,
+//     hide: 800
+//   }
+// });
+
+$('.menu .browse')
+  .popup({
+    inline: true,
+    // hoverable  : true,
+    position   : 'bottom right',
+    delay: {
+      show: 300,
+      hide: 800
+    }
+  });
